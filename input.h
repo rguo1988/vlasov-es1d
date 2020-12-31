53e6b1b61d615ee6bbb4b93a8be335f93d9617b5
piccolo04 <r.guo@live.com>
2020-12-31 11:08:08 +0800

    formation of kappa

--- input.h
+++ input.h
@@ -1,6 +1,6 @@
 //To Use this input by changing the filename to input.h
 /***********************************
- * simulation of $Pn^{-3}$ in Maxwellian plasma
+ * formation of kappa stationary distribution in an inhomogeneous plasma
  ***********************************/
 #ifndef _input_h
 #define _input_h
@@ -13,46 +13,50 @@ class Input
 {
   protected:
     //general parameters
-    const double k = 0.1;
-    const double L = 2.0 * M_PI / k; //simulaiton length
+    const double L = 10.0; //simulaiton length
+    const double k = 1 * 2.0 * M_PI / L;
     const double T = 1.0; //temperature
     const double m = 1.0;
     const double vmax = 5;
     const double e = -1.0;
 
     //definition of simulation constant
-    static const int nx = 301;//grid num is nx-1; grid point num is nx
+    static const int nx = 201;//grid num is nx-1; grid point num is nx
     static const int nx_grids = nx - 1;
-    static const int nv = 301;
+    static const int nv = 501;
     static const int nv_grids = nv - 1;
     const double dx = L / nx_grids;
     const double dv = 2 * vmax / nv_grids;
     const double dt = 0.02;
-    const int max_steps = 5000;
+    const int max_steps = 25000;
 
     //special parameters
-    const double delta = 0.003;
+    const double uae = 0.52;
+    const double uai = 0.5;
     const double kappa = 0.0;
+    const double u = 0.0;
     //data recording
     const string data_path = "./data/";
-    const int data_steps = 10;
+    const int data_steps = 10000;
     const int data_num = max_steps / data_steps + 1;
 
     double GetElecInitDistrib(double x, double v)
     {
-        double rx = 1.0 + delta * cos(k * x) ;
-
-        //non-adiabatic initial condition
-        double rv = sqrt(1.0 / (2 * M_PI * T)) * exp(- pow(v, 2) / (2 * T));
+        //f is distribution function normalized to 1, i.e. n=1
+        double rx = 1.0 + uae * cos(k * x) ;
+        //double rx = 1.0;
+        //double rv = sqrt(1.0 / (2 * M_PI * T * kappa)) * tgamma(kappa + 1.5) / tgamma(kappa + 1.0) * pow(1 + pow(v, 2) / (2 * T * kappa), -kappa-1.5);
+        //double rv = sqrt(1.0 / (2 * M_PI * T)) * exp(- pow(v - u, 2) / (2 * T));
+        double a = sqrt(3 * T / m);
+        double rv = 0;
+        if (v <= a && v >= -a)
+            rv = 1.0 / 2.0 / a;
         return rx * rv;
-        
-        //adiabatic initial condition
-        //double rv = sqrt(1.0 / (2 * M_PI * T)) * exp(- pow(v/rx, 2) / (2 * T));
-        //return 1.0 * rv;
     }
     double GetIonInitDistrib(double x, double v, double t)
     {
         double r = 1.0;
+        r = 1.0 + uai * cos(k * x);
         return r;
     }
 
@@ -61,7 +65,8 @@ class Input
         cout << "************************************" << endl;
         cout << "Special Parameters: " << endl;
         cout << " kappa = " << setw(8) << kappa
-             << "   delta = " << setw(8) << delta << endl;
+             << "   uae = " << setw(8) << uae
+             << "   uai = " << setw(8) << uai << endl;
         cout << "************************************" << endl;
     }
 };
