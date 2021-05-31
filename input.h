23a7a30b596c9cd7445384902e7b4768e51dcdc2
piccolo04 <r.guo@live.com>
2021-05-31 09:06:29 +0800

    EOS_diff_waves

--- input.h
+++ input.h
@@ -1,6 +1,6 @@
 //To Use this input by changing the filename to input.h
 /***********************************
-    electron acoustic wave: two-temperature electrons
+ * EOS under slow wave
  ***********************************/
 #ifndef _input_h
 #define _input_h
@@ -13,96 +13,121 @@ class Input
 {
   protected:
     //title
-    const string title = "electron acoustic wave: two-kappa electrons";
+    const string title = "EOS under slow waves";
+
+    //general parameters
+    const double k = 1.0;
+    const double L = 2 * M_PI / k; //simulaiton length
+    const double T = 1.0; //T for cold e
+    const double n = 1.0;
+    const double m = 1.0;
+    const double v_th = sqrt(2 * T / m);
+    const double vmax = 5;
+    const double e = -1.0;
+    const double l_D = sqrt(T / e / e);
+    const double w_p = sqrt(n*e*e / m);
+    const bool if_E_External = false;
 
     //special parameters
-    const double n = 1.0;
-    const double ns = 0.75;
-    const double nf = n - ns;
-    const double T = 1.0; //average temperature for all electrons
-    const double m = 1.0;
-    const double e = -1.0;
-    const double kappa_s = 1.501;
-    const double kappa_f = 20;
-    const double l_s = sqrt(T / ns);
-    const double l_f = sqrt(T / nf);
-    //
-    //general parameters
-    const double kls = 4.0;
-    const double k = kls / l_s;
-    const double L = 2 * M_PI / k; //simulaiton length
-    const double vmax = 10;
-    const double w_p = sqrt(n*e*e / m);
-    const double l_D = sqrt(T / n*e*e);
-    const bool if_E_External = false;
-    const double d = 1e-3;
+    const double d = 0.001;
+    const double v_0 = 0.5;
+    const double dv_p = 0.3;
+    const double dv_p_min = sqrt(4 * d / k / k);
+    const int n_p = 10;
+    const double Z = GetNormalization();
+    const double kappa = 2;
 
     //simulation constant
     static const int nx = 200;//grid num is nx-1; grid point num is nx
     static const int nx_grids = nx - 1;
-    static const int nv = 4000;
+    static const int nv = 2000;
     static const int nv_grids = nv - 1;
     const double dx = L / nx_grids;
     const double dv = 2 * vmax / nv_grids;
-    const double dt = 0.005;
-    const int max_steps = 8000;
-    const double dt_max = min(dx / vmax, dv * m * k / abs(e * d));
-
+    const double dt = 0.02;
+    const double dt_max = dv * m * k / abs(e) / d;
+    const int max_steps = 10000;
 
     //data recording
     const string data_path = "./data/";
     const int data_steps = max_steps;
     const int data_num = max_steps / data_steps + 1;
 
+
+    double f_p_unnorm(double v)
+    {
+        double f = f_eq(v);
+        double f0 = f_eq(v_0);
+        double D = 0.0;
+        if (v >= 0)
+            D = 1 + pow( ((v - v_0) / dv_p), n_p);
+        else
+            D = 1 + pow( ((v + v_0) / dv_p), n_p);
+        double N = f - f0;
+        double r = f - N / D;
+        return r;
+    }
+
+    double f_eq(double v)
+    {
+        //double rv = pow(1 + pow(v / v_th, 2), -kappa);
+        //double A = n / sqrt(M_PI * kappa) / v_th * tgamma(kappa) / tgamma(kappa - 0.5);
+        //double r = A * rv;
+        double r = exp(-v * v / v_th / v_th) / sqrt(M_PI) / v_th;
+        return r;
+    }
+
+    double GetNormalization()
+    {
+        double n = 0.0;
+        int N = 100000;
+        double v_lim = 100;
+        double dv_lim = 2 * v_lim / N;
+        for (int i = 0; i < N; i++)
+        {
+            n += f_p_unnorm(-v_lim + i * dv_lim) ;
+        }
+        n *= dv_lim;
+        return n;
+    }
+
     double GetElecInitDistrib(double x, double v)
     {
-        //f is distribution function normalized to 1, i.e. n=1
-        double rx = 1.0 + d * cos(k * x) ;
-        double As = ns / sqrt(2 * M_PI * T * (kappa_s - 1.5)) * tgamma(kappa_s) / tgamma(kappa_s - 0.5);
-        double Af = nf / sqrt(2 * M_PI * T * (kappa_f - 1.5)) * tgamma(kappa_f) / tgamma(kappa_f - 0.5);
-        double rvs = pow(1 + v * v / (2 * T * (kappa_s - 1.5)), -kappa_s);
-        double rvf = pow(1 + v * v / (2 * T * (kappa_f - 1.5)), -kappa_f);
-
-        return rx * (As * rvs + Af * rvf);
+        double wave = 1.0 + d * cos(k * x);
+        return wave * f_p_unnorm(v) / Z;
     }
 
     double GetIonInitDistrib(double x, double v, double t)
     {
-        double r = 1.0;
-        return r;
+        return 1.0;
     }
 
     double E_External(double x, double t)
     {
-        double r = 0.0;
-        //if (if_E_External)
-        //{
-        //double w = 1.009;
-        //double tau1 = 3 * dtau;
-        //double tau2 = 8 * dtau;
-        //double g1 = 1.0 / ( 1 + exp(-5 * (t - tau1) / dtau));
-        //double g2 = 1.0 / ( 1 + exp( 5 * (t - tau2) / dtau));
-        //r = 0.01 * (g1 + g2 - 1.0) * cos(k * x + w * t);
-        //}
-        return r;
+        //double T = 2 * M_PI / k / v_0;
+        //double dtau = 10 * T;
+        //double tau = 20 * T;
+        //double g = 1.0 / (
+        //1 + pow((t - tau) / dtau, 10)
+        //);
+        //double r = d * g * sin(k * x - k * v_0 * t);
+        return 0.0;
     }
 
     void PrintSpecialParameters()
     {
         cout << "************************************" << endl;
         cout << " Special Parameters: " << endl;
-        cout << "       nc = " << setw(8) << ns
-             << "       nh = " << setw(8) << nf << endl;
-        cout << "  kappa_c = " << setw(8) << kappa_s
-             << "  kappa_h = " << setw(8) << kappa_f << endl;
-        cout << "      l_s = " << setw(8) << l_s
-             << "      l_f = " << setw(8) << l_f << endl;
-        cout << "    k*l_s = " << setw(8) << k*l_s
-             << "    k*l_f = " << setw(8) << k*l_f << endl;
-        cout << "        d = " << setw(8) << d << endl;
+        cout << "       Z = " << setw(8) << Z
+             << "    v_th = " << setw(8) << v_th
+             << "     v_0 = " << setw(8) << v_0 << endl;
+        cout << "    dv_p = " << setw(8) << dv_p
+             << "     n_p = " << setw(8) << n_p << endl;
+        cout << "       d = " << setw(8) << d << endl;
         cout << "************************************" << endl;
         cout << " Parameters Max/Min: " << endl;
-        cout << "   dt_max = " << setw(8) << dt_max << endl;
+        cout << "  dt_max = " << setw(8) << dt_max << endl;
+        cout << "dv_p_min = " << setw(8) << dv_p_min << endl;
         cout << "************************************" << endl;
     }
 };
