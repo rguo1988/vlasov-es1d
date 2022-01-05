baac4b15fe6916434a312ffb54859ef9359979c3
piccolo04 <r.guo@live.com>
2022-01-05 13:43:18 +0800

    cutting the EH

--- inputs/ElectronHole/Coupled_Hole_EAW_Soliton.h
+++ inputs/ElectronHole/Coupled_Hole_EAW_Soliton.h
@@ -21,12 +21,12 @@ class Input
     const string title = "coupled hole and electron acoustic soliton";
 
     //general parameters
-    const double L = 50;
+    const double L = 100.0;
     const double k = 2.0 * M_PI / L; //simulaiton length
     const double vmax = 5.0;
     const double e = -1.0;
     const double n = 1.0;
-    #define _ions_motion false
+#define _ions_motion false
 
     const double Te = 1.0; //temperature
     const double me = 1.0;
@@ -42,27 +42,29 @@ class Input
 
     //special parameters
     const double u = 0.5;
-    const double vt_eh = 0.1;
+    const double nb = 0.05;
+    const double ub = 0.5;
+    const double vt_b = 0.1;
     const double b = -2.0;
-    const double d = 0.277835;//initial disturbance
+    const double d = 0.2778;//initial disturbance
     const double del = 4.5589;
 
     //simulation constant
     static const int nx = 1000;//grid num is nx-1; grid point num is nx
     static const int nx_grids = nx - 1;
-    static const int nv = 2000;
+    static const int nv = 3000;
     static const int nv_grids = nv - 1;
     const double dx = L / nx_grids;
     const double dv = 2 * vmax / nv_grids;
-    const double dt = 0.01;
-    const int max_steps = 1000;
+    const double dt = 0.02;
+    const int max_steps = 4000;
     const double dt_max = min(dx / vmax, dv * me * k / abs(e * d));
 
     VectorXd phi_sc;
 
     //data recording
     const string data_path = "./data/";
-    const int data_steps = max_steps;
+    const int data_steps = 500;
     const int data_num = max_steps / data_steps + 1;
 
     void CalculatePotentialSC()
@@ -71,16 +73,16 @@ class Input
         for(int i = 0; i < nx; i++)
         {
             double xp = (i * dx - L / 2.0) / del;
-            phi_sc(i) = 1.5 * d * pow(cosh(xp), -4);
+            phi_sc(i) = 1.0 * d * pow(cosh(xp), -4);
         }
-        phi_sc(0) = 0.0;
-        phi_sc(nx - 1) = 0.0;
+        //phi_sc(0) = 0.0;
+        //phi_sc(nx - 1) = 0.0;
         //construct laplace op
         //Dirichlet BC
         MatrixXd laplace(nx - 2, nx - 2);
-        laplace.diagonal() = VectorXd::Constant(nx - 2, -2);
-        laplace.diagonal(1) =  VectorXd::Constant(nx - 3, 1);
-        laplace.diagonal(-1) =  VectorXd::Constant(nx - 3, 1);
+        laplace.diagonal() = VectorXd::Constant(nx - 2, -2.0);
+        laplace.diagonal(1) =  VectorXd::Constant(nx - 3, 1.0);
+        laplace.diagonal(-1) =  VectorXd::Constant(nx - 3, 1.0);
         double dx2 = dx * dx;
 
         for(int i = 0; i < 10; i++)
@@ -95,22 +97,24 @@ class Input
                 {
                     temp += SchamelDistribution(phi_sc(j), -vmax + k * dv);
                 }
-                n(j) = temp * dv;
+                n(j) = (temp - 0.5 * (SchamelDistribution(phi_sc(j), -vmax) + SchamelDistribution(phi_sc(j), vmax))) * dv;
             }
             VectorXd r = (VectorXd::Ones(nx - 2) - n.segment(1, nx - 2)) * dx2 + laplace * phi_sc.segment(1, nx - 2);
             VectorXd m = VectorXd::Ones(nx - 2).array() * 2.0 + (n.segment(2, nx - 2) - n.segment(0, nx - 2)).array() / (phi_sc.segment(2, nx - 2) - phi_sc.segment(0, nx - 2)).array() * dx2;
             MatrixXd nr_mat(nx - 2, nx - 2);
             nr_mat.setZero();
             nr_mat.diagonal() = m;
-            nr_mat.diagonal(1) = VectorXd::Constant(nx - 3, -1);
-            nr_mat.diagonal(-1) = VectorXd::Constant(nx - 3, -1);
-            VectorXd dphi = nr_mat.fullPivLu().solve(r);
+            nr_mat.diagonal(1) = VectorXd::Constant(nx - 3, -1.0);
+            nr_mat.diagonal(-1) = VectorXd::Constant(nx - 3, -1.0);
+            //VcctorXd dphi = nr_mat.fullPivLu().solve(r);
+            VectorXd dphi = nr_mat.ldlt().solve(r);
             phi_sc.segment(1, nx - 2) += dphi;
 
             double err = ((dphi.array() / phi_sc.segment(1, nx - 2).array()) * (dphi.array() / phi_sc.segment(1, nx - 2).array())).sum();
-            if(err < 1e-5)
+            if(err <= 1e-5)
             {
                 cout << "Self consistent potential is solved! Iteration counts = "  << i << endl;
+                cout << "err = " << err << endl;
                 break;
             }
         }
@@ -120,20 +124,21 @@ class Input
     {
         double v_waveframe = v;
         double w = v * v / 2.0 - phi;
-        double r1 = 0.0;
+        double r = 0.0;
         if (v_waveframe <= -sqrt(2.0 * phi))
-            r1 = exp(- 0.5 * pow(-sqrt(2 * w) + u, 2));
+            r = exp(- 0.5 * pow(-sqrt(2 * w) + u, 2));
         else if (v_waveframe > sqrt(2.0 * phi))
-            r1 = exp(- 0.5 * pow( sqrt(2 * w) + u, 2));
+            r = exp(- 0.5 * pow( sqrt(2 * w) + u, 2));
         else
-            r1 = exp(-b * w - u * u / 2.0);
-        double r2 = exp(-0.5 * pow((v + u) / vt_eh, 2)) / sqrt(2.0 * M_PI) / vt_eh;
-        return 0.5 * (r1 / sqrt(2.0 * M_PI) + r2);
+            r = exp(-b * w - u * u / 2.0);
+        return r / sqrt(2.0 * M_PI);
     }
 
     double GetElecInitDistrib(double x, double v)
     {
-        return SchamelDistribution(phi_sc(x / dx), v);
+        double r2 = exp(-0.5 * pow((v + ub) / vt_b, 2)) / sqrt(2.0 * M_PI) / vt_b;
+        return (1 - nb) * SchamelDistribution(phi_sc(x / dx), v) + nb * r2;
+        //return SchamelDistribution(phi_sc(x / dx), v);
     }
 
     double GetIonInitDistrib(double x, double v)
@@ -152,10 +157,10 @@ class Input
     {
         cout << "************************************" << endl;
         cout << " Special Parameters: " << endl;
-        cout << "     beta = " << setw(6) << b
-             << "        u = " << setw(6) << u << endl;
-        cout << "        d = " << setw(6) << d
-             << "      del = " << setw(6) << del << endl;
+        cout << "        u = " << setw(6) << u
+             << "       ub = " << setw(6) << ub << endl;
+        cout << "       nb = " << setw(6) << nb
+             << "     vt_b = " << setw(6) << vt_b << endl;
         cout << "************************************" << endl;
         cout << " Parameters Max/Min: " << endl;
         cout << "   dt_max = " << setw(6) << dt_max << endl;
